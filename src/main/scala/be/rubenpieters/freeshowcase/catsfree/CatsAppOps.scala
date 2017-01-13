1ae2e141fd21795400ce01483e339a9c117b3d95
rubenpieters <ruben.pieters2@pandora.be>
2017-01-13 17:09:13 +0100

    wip filtering irrelevant search results

--- src/main/scala/be/rubenpieters/freeshowcase/catsfree/CatsAppOps.scala
+++ src/main/scala/be/rubenpieters/freeshowcase/catsfree/CatsAppOps.scala
@@ -2,7 +2,7 @@ package be.rubenpieters.freeshowcase.catsfree
 
 import cats._
 import cats.implicits._
-import be.rubenpieters.freeshowcase.{MusicDsl, Playlist, PlaylistDsl, VideoDsl}
+import be.rubenpieters.freeshowcase._
 import cats.data.Coproduct
 import cats.free.Free
 
@@ -12,14 +12,15 @@ import cats.free.Free
 class CatsAppOps[F[_]](implicit P: CatsPlaylistOps[F], V: CatsVideoOps[F], M: CatsMusicOps[F]) {
   def createPlaylistFromFavoriteTracks(user: String): Free[F, Playlist] = for {
     tracks <- M.favoriteTracksForUser(user)
-    trackSearchTerms = tracks.map(track => s"${track.artist} - ${track.title}")
+    trackSearchTerms = tracks.map(track => List(track.artist, track.title))
     playlist <- createPlaylistFromLiteralList(trackSearchTerms)
   } yield playlist
 
-  def createPlaylistFromLiteralList(list: List[String]): Free[F, Playlist] = for {
-    searchResults <- list.traverseU(V.literalSearch)
+  def createPlaylistFromLiteralList(list: List[List[String]]): Free[F, Playlist] = for {
+    searchResults <- list.traverseU(r => V.literalSearch(r.mkString(" - ")))
+    relevantSearchResults = searchResults.zip(list).map{ case (videoSearchResult, terms) => VideoSearchResult(videoSearchResult.results.filter(t => Video.containsTermsFully(t.title, terms)))}
     newPlaylist <- P.createPlaylist()
-    updatedPlaylist <- searchResults.traverseU(searchResult => P.addVideo(searchResult.results.head, newPlaylist))
+    updatedPlaylist <- relevantSearchResults.traverseU(searchResult => P.addVideo(searchResult.results.head, newPlaylist))
   } yield newPlaylist
 }
 
